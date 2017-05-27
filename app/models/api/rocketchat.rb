f04eecfa14222ded8d2952fd76ceb8baed3d7ce2
derori <derori.derori@gmail.com>
2017-05-27 22:27:26 +0900

    ちょっとdomainなどを共通化。

--- app/models/api/rocketchat.rb
+++ app/models/api/rocketchat.rb
@@ -5,23 +5,38 @@ class Api::Rocketchat
     end
     @instance = instance
     @user = User.where(instance_id: instance.id).where.not(token: nil).first
+    @site = "https://#{@instance.host}"
   end
 
   def login(user, pass)
-    conn = Faraday.new(:url => "https://#{@instance.host}", :ssl => {:verify => false})
-    req = conn.post '/api/v1/login', username: user, password: pass
+    uri = '/api/v1/login'
+    conn = Faraday.new(:url => @site, :ssl => {:verify => false})
+    req = conn.post uri, {username: user,
+                          password: pass}
     res = JSON.parse(req.body)
+
     @user = User.find_or_create_by(
-      name: user,
+      name:        user,
       instance_id: @instance.id
     )
-    @user.key = res['data']['userId']
+    @user.key   = res['data']['userId']
     @user.token = res['data']['authToken']
     @user.save!
     @user
   end
 
   def channels
+    conn = Faraday.new(:url => "https://#{@instance.host}", :ssl => {:verify => false})
+    req = conn.post '/api/v1/channels.list', {instance_id: @instance.id,
+                                              method: method,
+                                              req: nil}
+    res = JSON.parse(req.body)
+    @user = User.find_or_create_by(
+      name: user,
+      instance_id: @instance.id
+    )
+
+
     path = '/api/v1/channels.list'
     method = 'GET'
     params = {
