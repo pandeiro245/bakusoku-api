b6ea86936a6f439cc56726744c5f8c059f8c82b9
derori <derori.derori@gmail.com>
2017-05-28 11:51:09 +0900

    add method login, channels, history  rokectchat

--- app/models/api/rocketchat.rb
+++ app/models/api/rocketchat.rb
@@ -1,17 +1,22 @@
 class Api::Rocketchat
   def initialize(instance = nil)
+    provider_name = self.class.to_s.split('::').last.downcase
     unless instance.present?
-      instance = Instance.find_by(provider_name: 'rocketchat')
+      instance = Instance.find_or_create_by(provider_name: provider_name)
     end
     @instance = instance
+    @instance.save!
+
     @user = User.where(instance_id: instance.id).where.not(token: nil).first
     @site = "https://#{@instance.host}"
   end
 
+  # rocketchar login
   def login(user, pass)
-    uri = '/api/v1/login'
+    path = '/api/v1/login'
+    # TODO: delete verify => false
     conn = Faraday.new(:url => @site, :ssl => {:verify => false})
-    req = conn.post uri, {username: user,
+    req = conn.post path, {username: user,
                           password: pass}
     res = JSON.parse(req.body)
 
@@ -25,49 +30,43 @@ class Api::Rocketchat
     @user
   end
 
+  # get channles list
   def channels
-    conn = Faraday.new(:url => "https://#{@instance.host}", :ssl => {:verify => false})
-    req = conn.post '/api/v1/channels.list', {instance_id: @instance.id,
-                                              method: method,
-                                              req: nil}
-    res = JSON.parse(req.body)
-    @user = User.find_or_create_by(
-      name: user,
-      instance_id: @instance.id
-    )
-
-
     path = '/api/v1/channels.list'
-    method = 'GET'
-    params = {
-      instance_id: @instance.id,
-      path: path,
-      method: method,
-      req: nil,
-    }
-    datum = Datum.find_by(params)
-    unless datum
-      res = `curl -H "X-Auth-Token: #{@user.token}" \
-           -H "X-User-Id: #{@user.key}" \
-                https://#{@instance.host}#{path}`
-      params[:res] = res
-      datum = Datum.create(params)
+    # TODO: delete verify => false
+    conn = Faraday.new(:url => @site, :ssl => {:verify => false})
+
+    res = conn.get do |req|
+      req.url(path)
+      req.headers = {'Content-Type' => 'application/json',
+                     'X-Auth-Token' => @user.token,
+                     'X-User-Id'    => @user.key
+                    }
     end
-    JSON.parse(datum.res)
+    return JSON.parse(res.body)
   end
 
   def histries
+
     path = '/api/v1/channels.history'
-    method = 'GET'
-    channels.each['channels'].each do |channel|
-      room_id = channel['_id']
-      params = {
-        instance_id: @instance.id,
-        path: path,
-        method: method,
-        req: {roomId: room_id}.to_json,
-      }
+    # TODO: delete verify => false
+    conn = Faraday.new(:url => @site, :ssl => {:verify => false})
+
+    histries = []
+
+    self.channels()['channels'].each do |c|
+      res = conn.get do |req|
+        req.url(path)
+        req.headers = {'Content-Type' => 'application/json',
+                       'X-Auth-Token' => @user.token,
+                       'X-User-Id'    => @user.key
+                      }
+        req.params['roomId'] = c['_id']
+      end
+
+      histries.push(JSON.parse(res.body))
     end
+    return histries
   end
 end
 
